cd86f8fce941e3d77e079d84ea464b599475afcb
altescy <altescy@gmail.com>
2018-11-01 17:19:38 +0900

    less return value

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ const vm = new Vue({
     axios.get("https://qiita.com/api/v2/items?page=1&per_page=20&query=user%3Aaltescy")
     .then(response => {
         this.results = response.data.map(e => {
-            e.date = (new Date(e.created_at)).toLocaleDateString();
-            e.abst = e.rendered_body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').slice(0, 200) + "...";
-            return e;
+            return {
+                "title": e.title,
+                "created_at": e.created_at,
+                "date": (new Date(e.created_at)).toLocaleDateString(),
+                "abst": e.rendered_body.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').slice(0, 200) + "...",
+            };
         });
         this.results.sort(function(a, b) {
           if(a.created_at < b.created_at) return 1;
